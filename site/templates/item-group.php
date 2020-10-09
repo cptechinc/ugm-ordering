090b7f209efcc2f591ac2a6bf6ef38b7634645ae
Paul Gomez <pauldro952@hotmail.com>
2020-10-09 10:04:57 -0500

    Item Permissions on the group page

--- site/templates/item-group.php
+++ site/templates/item-group.php
@@ -1,10 +1,14 @@
 <?php
-	$dpluspricing = $modules->get('ItemSearchDplus');
-	$page->searchurl = $pages->get('template=items-search')->url;
-	$page->carturl = $pages->get('template=cart')->url;
-	$items = $page->get_stocked_items();
+	if ($user->has_itemgroup()) {
+		$dpluspricing = $modules->get('ItemSearchDplus');
+		$page->searchurl = $pages->get('template=items-search')->url;
+		$page->carturl = $pages->get('template=cart')->url;
+		$items = $page->get_stocked_items();
 
-	$page->body .= $config->twig->render('items/search/form.twig', ['page' => $page]);
-	$page->body .= $config->twig->render('items/list.twig', ['page' => $page, 'items' => $items, 'dpluspricing' => $dpluspricing]);
+		$page->body .= $config->twig->render('items/search/form.twig', ['page' => $page]);
+		$page->body .= $config->twig->render('items/list.twig', ['page' => $page, 'items' => $items, 'dpluspricing' => $dpluspricing]);
 
-	include('./basic-page.php');
+		include('./basic-page.php');
+	} else {
+		throw new Wire404Exception();
+	}
