df97d55595186a64220eafc5d1666a33a9e3a10e
Paul Gomez <pauldro952@hotmail.com>
2020-10-09 09:57:27 -0500

    Hide Groups User doesn't have access to

--- site/templates/items-menu.php
+++ site/templates/items-menu.php
@@ -1,6 +1,13 @@
 <?php
 	$page->searchurl = $pages->get('template=items-search')->url;
+	
+	if ($user->has_item_restrictions()) {
+		$groups = implode('|', $user->restricted_itemgroups());
+		$itemgroups = $page->children("template=item-group,groupcode=$groups");
+	} else {
+		$itemgroups = $page->children('template=item-group');
+	}
 	$page->body .= $config->twig->render('items/search/form.twig', ['page' => $page]);
-	$page->body .= $config->twig->render('items/menu.twig', ['page' => $page, 'itemgroups' => $page->children('template=item-group')]);
+	$page->body .= $config->twig->render('items/menu.twig', ['page' => $page, 'itemgroups' => $itemgroups]);
 
 	include('./basic-page.php');
