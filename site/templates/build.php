d7d693e5290819e886dfdf51ed361678c3d1de62
Paul <pauldro952@outlook.com>
2021-03-04 09:33:37 -0600

    Update UgmOrderingPagesItem functions

--- site/templates/build.php
+++ site/templates/build.php
@@ -3,6 +3,6 @@
 	$itemgroupm = $modules->get('UgmOrderingPagesItemGroup');
 	$itemsm = $modules->get('UgmOrderingPagesItem');
 	$result['groups'] = $itemgroupm->update_itemgroup_pages();
-	$result['items'] = $itemsm->update_item_pages();
+	$result['items'] = $itemsm->updatePages();
 
 	echo json_encode($result);
