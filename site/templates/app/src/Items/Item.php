edd9654905e7950c2811947afe9b515ddba2d2da
Paul <pauldro952@outlook.com>
2021-11-22 08:49:10 -0600

    Update Item.php

--- site/templates/app/src/Items/Item.php
+++ site/templates/app/src/Items/Item.php
@@ -1,15 +1,18 @@
 <?php namespace Controllers\Items;
 // Mvc Controllers
 use Controllers\Base;
-
+// Dplus Warehouse Management
 use Dplus\Wm\Inventory\Lots\Lookup\ExcludePackBin as WhseLots;
+// Dplus Document Managment
+use Dplus\DocManagement\Finders\Lt\Img as Docm;
+
 
 class Item extends Base {
 	public static function index($data) {
 		$whseLots = new WhseLots();
 		$whseLots->setWhseID(1);
 		$lots = $whseLots->getLotsByItemid(self::pw('page')->itemid);
-		
-		return self::pw('config')->twig->render('items/item/display.twig', ['lots' => $lots]);
+
+		return self::pw('config')->twig->render('items/item/display.twig', ['lots' => $lots, 'lotm' => $lotm]);
 	}
 }
