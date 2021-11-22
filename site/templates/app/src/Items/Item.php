73b21a730138923936925f3c52dc3353f2a52461
Paul <pauldro952@outlook.com>
2021-11-22 13:39:53 -0600

    Lot images

--- site/templates/app/src/Items/Item.php
+++ site/templates/app/src/Items/Item.php
@@ -3,16 +3,54 @@
 use Dplus\Wm\Inventory\Lots\Lookup\ExcludePackBin as WhseLots;
 // Dplus Document Managment
 use Dplus\DocManagement\Finders\Lt\Img as Docm;
+use Dplus\DocManagement\Folders;
+use Dplus\DocManagement\Copier;
 // Mvc Controllers
 use Controllers\Base;
 
 
 class Item extends Base {
+	private static $docm;
+
 	public static function index($data) {
 		$whseLots = new WhseLots();
 		$whseLots->setWhseID(1);
 		$lots = $whseLots->getLotsByItemid(self::pw('page')->itemid);
+		$docm = self::getDocm();
+		self::copyImage($data, $lots);
 
-		return self::pw('config')->twig->render('items/item/display.twig', ['lots' => $lots, 'lotm' => $whseLots]);
+		return self::pw('config')->twig->render('items/item/display.twig', ['lots' => $lots, 'docm' => $docm]);
+	}
+
+	private static function copyImage($data, $lots) {
+		$docm = self::getDocm();
+
+		foreach ($lots as $lot) {
+			if ($docm->hasImage($lot['lot'])) {
+				$file = $docm->getImage($lot['lot']);
+				$folder = Folders::getInstance()->folder($file->folder);
+				$copier = Copier::getInstance();
+				$copier->useDocVwrDirectory();
+
+				if ($copier->isInDirectory($file->filename) === false) {
+					echo $copier->copyFile($folder->directory, $file->filename) ? 'true' : 'false';
+				}
+			}
+		}
+	}
+
+/* =============================================================
+	Init
+============================================================= */
+	public static function initHooks() {
+		$m = self::pw('modules')->get('UgmOrderingPages');
+
+	}
+
+	private static function getDocm() {
+		if (empty(self::$docm)) {
+			self::$docm = new Docm();
+		}
+		return self::$docm;
 	}
 }
