f895b9096a01751942a4b961fea05fc70b01c260
Paul <pauldro952@outlook.com>
2021-11-22 11:07:53 -0600

    Update to use MVC

--- site/templates/cart.php
+++ site/templates/cart.php
@@ -1,32 +1,15 @@
 <?php
-	$checkout = $modules->get('Checkout');
-	$checkout->deleteOldBilling();
-	$cart = $modules->get('Cart');
-	$qnotes = $modules->get('QnotesCart');
-	$rm = strtolower($input->requestMethod());
-	$values = $input->$rm;
+	use Controllers\Cart as Controller;
 
-	if ($values->action) {
-		$cart->process_input($input);
-		$url = $values->page ? $values->text('page') : $page->url;
-		$session->redirect($url);
-	}
+	$routes = [
+		['GET',  '', Controller::class, 'index'],
+		['POST',  '', Controller::class, 'handleCRUD'],
+	];
 
-	if ($session->response_cart) {
-		$page->body .= $config->twig->render('util/dplus-response.twig', ['response' => $session->response_cart]);
-		$session->remove('response_cart');
-	}
+	$router = new Mvc\Router();
+	$router->setRoutes($routes);
+	$router->setRoutePrefix($page->url);
+	$page->body = $router->route();
+	$page->show_breadcrumbs = false;
 
-	if ($session->response_qnote) {
-		$page->body .= $config->twig->render('util/dplus-response.twig', ['response' => $session->response_qnote]);
-		$session->remove('response_qnote');
-	}
-
-	$page->body .= $config->twig->render('cart/cart.twig', ['page' => $page, 'user' => $user, 'cart' => $cart, 'qnotes' => $qnotes]);
-	$page->body .= $config->twig->render('cart/notes/modal.twig', ['page' => $page, 'qnotes' => $qnotes]);
-	$page->js   .= $config->twig->render('cart/js.twig', ['page' => $page, 'cart' => $cart]);
-	$page->js   .= $config->twig->render('cart/lookup.js.twig', ['page' => $page, 'cart' => $cart]);
-	$page->js   .= $config->twig->render('cart/notes/js.twig', ['page' => $page, 'cart' => $cart]);
-	$config->scripts->append(hash_templatefile('scripts/lib/jquery-validate.js'));
-
-	include('./basic-page.php');
+	include __DIR__ . "/basic-page.php";
