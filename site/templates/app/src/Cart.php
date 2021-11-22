388afc19fc9da29cdae1250a7a06b3665e76d3d4
Paul <pauldro952@outlook.com>
2021-11-22 14:17:10 -0600

    Update Cart.php

--- site/templates/app/src/Cart.php
+++ site/templates/app/src/Cart.php
@@ -48,6 +48,9 @@ class Cart extends Base {
 		$page   = self::pw('page');
 		$cart   = CartCRUD::getInstance();
 
+		$checkout = self::pw('modules')->get('Checkout');
+		$checkout->deleteOldBilling();
+
 		$page->js .= $config->twig->render('cart/js.twig');
 		$page->js .= $config->twig->render('cart/lookup.js.twig');
 		$page->js .= $config->twig->render('cart/notes/js.twig');
@@ -58,13 +61,38 @@ class Cart extends Base {
 /* =============================================================
 	Display Functions
 ============================================================= */
-	public static function displayCart($data) {
+	private static function displayCart($data) {
 		$cart   = CartCRUD::getInstance();
 		$qnotes = self::pw('modules')->get('QnotesCart');
 
 		$html = '';
+		$html .= self::displayResponseCart($data);
+		$html .= self::displayResponseQnotes($data);
 		$html .= self::pw('config')->twig->render('cart/cart.twig', ['cart' => $cart, 'qnotes' => $qnotes]);
 		$html .= self::pw('config')->twig->render('cart/notes/modal.twig', ['cart' => $cart, 'qnotes' => $qnotes]);
+		return $html;
+	}
+
+	private static function displayResponseCart($data) {
+		$cart     = CartCRUD::getInstance();
+		$response = $cart->getResponse();
+		$html = '';
+
+		if ($response) {
+			$html .= $config->twig->render('util/dplus-response.twig', ['response' => $response]);
+			$cart->deleteResponse();
+		}
+		return $html;
+	}
+
+	private static function displayResponseQnotes($data) {
+		$session = self::pw('session');
+		$html = '';
+
+		if ($session->response_qnote) {
+			$html .= $config->twig->render('util/dplus-response.twig', ['response' => $session->response_qnote]);
+			$session->remove('response_qnote');
+		}
 		return $html;
 	}
 
