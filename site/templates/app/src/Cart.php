75d1f62ecff321281a19b9cad89c5674070c52d3
Paul <pauldro952@outlook.com>
2021-11-22 11:08:03 -0600

    Update Cart.php

--- site/templates/app/src/Cart.php
+++ site/templates/app/src/Cart.php
@@ -1,11 +1,40 @@
 <?php namespace Controllers;
-
+// Dplus Ecomm
+use Dplus\Ecomm\Cart as CartCRUD;
 // Mvc Controllers
 use Controllers\Base;
 
 
 class Cart extends Base {
+/* =============================================================
+	Index Functions
+============================================================= */
 	public static function index($data) {
+		return self::cart($data);
+	}
 
+	private static function cart($data) {
+		$config = self::pw('config');
+		$page   = self::pw('page');
+		$cart   = CartCRUD::getInstance();
+
+		$page->js .= $config->twig->render('cart/js.twig');
+		$page->js .= $config->twig->render('cart/lookup.js.twig');
+		$page->js .= $config->twig->render('cart/notes/js.twig');
+		$config->scripts->append(self::pw('modules')->get('FileHasher')->getHashUrl('scripts/lib/jquery-validate.js'));
+		return self::displayCart($data);
+	}
+
+/* =============================================================
+	Display Functions
+============================================================= */
+	public static function displayCart($data) {
+		$cart   = CartCRUD::getInstance();
+		$qnotes = self::pw('modules')->get('QnotesCart');
+
+		$html = '';
+		$html .= self::pw('config')->twig->render('cart/cart.twig', ['cart' => $cart, 'qnotes' => $qnotes]);
+		$html .= self::pw('config')->twig->render('cart/notes/modal.twig', ['cart' => $cart, 'qnotes' => $qnotes]);
+		return $html;
 	}
 }
