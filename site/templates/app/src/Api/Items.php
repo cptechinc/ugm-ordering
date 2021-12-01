b1c76b70d904acf972ca8f210e661c77804184c7
Paul <pauldro952@outlook.com>
2021-12-01 13:25:49 -0600

    Update Items.php

--- site/templates/app/src/Api/Items.php
+++ site/templates/app/src/Api/Items.php
@@ -1,11 +1,14 @@
 <?php namespace Controllers\Api;
 // Dplus Min
 use Dplus\Min\Itm;
+// Dplus Ecomm
+use Dplus\Ecomm\Items\Available\Items as ItemInventory;
 
 class Items extends Base {
 	public static function getItem($data) {
 		self::sanitizeParametersShort($data, ['itemID|text']);
 		$itm = Itm::getInstance();
+		$inventory = ItemInventory::getInstance();
 
 		if (empty($data->itemID) || $itm->exists($data->itemID) === false) {
 			return false;
@@ -17,7 +20,8 @@ class Items extends Base {
 			'description2' => $item->description2,
 			'uom' => [
 				'sale' => $item->uom_sale,
-			]
+			],
+			'available'   => $inventory->getAvailability($data->itemID)
 		];
 		return $json;
 	}
