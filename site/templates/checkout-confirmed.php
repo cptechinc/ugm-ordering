fea84631377629d19c00d9a58ecb97c9b3e9474f
Paul <pauldro952@outlook.com>
2022-01-24 11:59:20 -0600

    Clear cart, grab order number from orders table

--- site/templates/checkout-confirmed.php
+++ site/templates/checkout-confirmed.php
@@ -8,6 +8,14 @@
 		$session->setFor('order', 'created', $billing->orders);
 	}
 
+	if ($billing->has_error() === false && empty($billing->orders)) {
+		$q = SalesOrderQuery::create();
+		$q->select(SalesOrder::aliasproperty('ordernumber'));
+		$q->filterByCustid($billing->custid);
+		$q->filterByShiptoid($billing->shiptoid);
+		$q->filterByEmail($billing->email);
+		$session->setFor('order', 'created', $q->findOne());
+	}
 
 	if ($session->response_checkout) {
 		$page->body .= $config->twig->render('util/dplus-response.twig', ['response' => $session->response_checkout]);
@@ -16,6 +24,8 @@
 
 	if ($billing->has_error()) {
 		$page->body .= $config->twig->render('util/dplus-response.twig', ['response' => DplusResponse::create_error($billing->ermes)]);
+	} else {
+		$modules->get('Cart')->clear();
 	}
 
 	$page->body .= $config->twig->render('checkout/confirmation.twig', ['page' => $page, 'user' => $user, 'billing' => $billing]);
